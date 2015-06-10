2504e02922726b0643a455924d9ab6bd7663a8ac
phamvanthang310 <phamvanthang310@gmail.com>
2015-06-10 11:41:46 +0700

    fix error

--- app/src/main/java/com/example/thang/mobile_dating_app_v20/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/thang/mobile_dating_app_v20/Activity/RegisterActivity.java
@@ -70,7 +70,7 @@ public class RegisterActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_register, menu);
+        //getMenuInflater().inflate(R.menu.menu_register, menu);
         return true;
     }
 
